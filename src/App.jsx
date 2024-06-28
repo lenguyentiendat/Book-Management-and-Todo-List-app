596fa8f8bd33493db0599484342247ab80d2a464
lenguyentiendat <20521167@ms.uit.edu.vn>
2024-06-28 17:56:54 +0700

    Update video 28

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
 // () => {}
 // component = html + css + js
-const MyComponent = () => {
-  return (
-    <div>datlnt & cobra update</div>
-  )
-}
+
 const App = () => {
   const [count, setCount] = useState(0)
 
@@ -24,7 +22,9 @@ const App = () => {
         </a>
       </div>
       <h1>Hello Mother Fucker</h1>
-      <MyComponent/>
+      <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,12 @@
+const MyComponent = () => {
+  return (
+    <div>datlnt & cobra update</div>
+  )
+}
+
+const FakeComponent = () => {
+  return (
+    <div>This is my show, my ground</div>
+  )
+}
+export default MyComponent
