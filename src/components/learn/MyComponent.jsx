0334efdca21dcc9c8bd94d382adb65d9dfc7554d
lenguyentiendat <20521167@gm.uit.edu.vn>
2024-06-28 18:40:56 +0700

    Update video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,16 @@
+//fragment
+// JSX: 1 parent 
+import './style.css'
 const MyComponent = () => {
   return (
-    <div>datlnt & cobra update</div>
+    <>
+      <div>datlnt & cobra update</div>
+      <div className="child"
+        style={
+          { borderRadius: "50px" }
+        }
+      >child</div>
+    </>
   )
 }
 
