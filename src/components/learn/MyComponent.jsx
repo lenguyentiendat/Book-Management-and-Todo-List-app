c100d4b6ee2385aa404e81285e979449ba11f94c
lenguyentiendat <20521167@gm.uit.edu.vn>
2024-06-29 15:16:15 +0700

    Update video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,13 +2,23 @@
 // JSX: 1 parent 
 import './style.css'
 const MyComponent = () => {
+  // const nameEmployee = "DatLNT@gmail.com"
+  // const nameEmployee = 69
+  // const nameEmployee = true
+  // const nameEmployee = undefined
+  const nameEmployee = [1, 2, 3]
+  // const nameEmployee = {
+  //   age: 23,
+  //   name: 'DatLNT',
+  // }
+
+
   return (
     <>
-      <div>datlnt & cobra update</div>
+      <div>{JSON.stringify(nameEmployee)} & cobra update</div>
+      <div>{console.log("DatLNT")}</div>
       <div className="child"
-        style={
-          { borderRadius: "50px" }
-        }
+        style={{ borderRadius: "50px" }}
       >child</div>
     </>
   )
