5af8100d2fb86b12d4cd846adb0255589995a458
lenguyentiendat <20521167@gm.uit.edu.vn>
2024-07-10 12:15:13 +0700

    Update video 46

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import UsersPage from './pages/users.jsx';
+import ProductsPage from './pages/products.jsx';
+import './styles/global.css'
 
 const router = createBrowserRouter([
   {
@@ -13,19 +18,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>Login Page</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>Register page</div>,
+    element: <RegisterPage />,
   },
   {
     path: "/users",
-    element: <div>Users page</div>,
+    element: <UsersPage />,
   },
   {
     path: "/products",
-    element: <div>Products page</div>,
+    element: <ProductsPage />,
   },
 ]);
 
