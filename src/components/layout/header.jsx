0e98af2227ab9454226c3256d8f96bee4b793b21
lenguyentiendat <20521167@gm.uit.edu.vn>
2024-07-11 23:42:53 +0700

    Update video 50

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,11 @@
 import './header.css';
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <ul>
-            <li><Link class="active" to="/">Home</Link></li>
-            <li><Link to="/users">Users</Link></li>
-            <li><Link to="/products">Products</Link></li>
+            <li><NavLink class="active" to="/">Home</NavLink></li>
+            <li><NavLink to="/users">Users</NavLink></li>
+            <li><NavLink to="/products">Products</NavLink></li>
         </ul>
     )
 }
