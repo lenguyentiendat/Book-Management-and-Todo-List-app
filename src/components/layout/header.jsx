7a9e132bebb3e18c8bd6882580b5fbe47486404a
lenguyentiendat <20521167@gm.uit.edu.vn>
2024-07-11 23:29:49 +0700

    Update video 49

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,10 +1,11 @@
 import './header.css';
+import { Outlet, Link } from "react-router-dom";
 const Header = () => {
     return (
         <ul>
-            <li><a class="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/products">Products</a></li>
+            <li><Link class="active" to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/products">Products</Link></li>
         </ul>
     )
 }
