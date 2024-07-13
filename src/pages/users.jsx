a79b241f3e934225d9be70225457e04aaca65704
lenguyentiendat <20521167@gm.uit.edu.vn>
2024-07-14 00:25:47 +0700

    Update video 67

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,6 +1,12 @@
+import FormUser from "../components/users/form.user"
+import TableUsers from "../components/users/table.user"
+
 const UsersPage = () => {
     return (
-        <div>Users page</div>
+        <div style={{ padding: "20px" }}>
+            <FormUser />
+            <TableUsers />
+        </div>
     )
 }
 
